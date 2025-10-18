b8550022e781bde12a4278daacb74bf5e8ee1b95
Nigina Zaripova <zaripovanigina1@gmail.com>
2025-10-18 12:29:35 +0300

    fix bags

--- main.js
+++ main.js
@@ -6,25 +6,25 @@
 // 5. Добавить кнопку, при нажатии на которую мы будем менять её цвет с одного на другой. При повторном нажатии цвет меняется с второго на первый. Цвета выбираем по желанию.
 
 // 1.Перекрашиваем первую карточку
-const changeColorButtonCard = document.querySelector('.button-card');
-const productCard = document.querySelector('.product-card');
+const changeColorFirstCardButton = document.querySelector('.button-card');
+const firstProductCard = document.querySelector('.product-card');
 const colorChangePink = "#e5bce1";
 const colorChangeViolet = "#cacaf4";
 
-changeColorButtonCard.addEventListener('click', () => {
-    productCard.style.background = colorChangePink;
+changeColorFirstCardButton.addEventListener('click', () => {
+    firstProductCard.style.background = colorChangePink;
 });
 
 // 2. Перекрашиваем все карточки
-const changeColorButtonCardAll = document.querySelector('.button-cards');
-const productCardAll = document.querySelectorAll('.product-card');
+const changeColorAllCardButton = document.querySelector('.button-cards');
+const allproductCards = document.querySelectorAll('.product-card');
 
-changeColorButtonCardAll.addEventListener('click', () => {
-    productCardAll.forEach((card) => card.style.background = colorChangeViolet);
+changeColorAllCardButton.addEventListener('click', () => {
+    allproductCards.forEach((card) => card.style.background = colorChangeViolet);
 });
 // 3. Открываем Google
-const openButtonGoogle = document.querySelector('#open-google');
-openButtonGoogle.addEventListener('click', openGoogle);
+const openGoogleButton = document.querySelector('#open-google');
+openGoogleButton.addEventListener('click', openGoogle);
 
 function openGoogle() {
     const answer = confirm('Вы действительно хотите открыть эту страницу?');
@@ -35,15 +35,15 @@ function openGoogle() {
     }
 };
 // 4. Вывод консоль лог 
-const titlePager = document.querySelector('.title-pager');
+const mainTitle = document.querySelector('.title-pager');
 
-titlePager.addEventListener('mouseover', () => outputConsoleLog('Выбери свой продукт'));
+mainTitle.addEventListener('mouseover', () => outputConsoleLog(mainTitle.textContent));
 
 function outputConsoleLog(message) {
     console.log(message);
-}
+};
 // 5.При клике меняет цвет кнопки
 const changeColorButton = document.querySelector('#change-color-button');
 changeColorButton.addEventListener('click', () => {
-    changeColorButton.classList.toggle('button--color');
-})
+    changeColorButton.classList.toggle('button--color-pink');
+});
